624768a50212a4030145b9602fc99ec4cb64f447
samaahmed1 <sama34777@gmail.com>
2025-02-18 20:13:02 +0200

    Add: ButtonWhite Component

---
+++ src/components/ButtonWhite.jsx
@@ -0,0 +1,4 @@
+function ButtonWhite({ content }) {
+  return <button className="text-[#212529] border border-[#212529] py-1 px-3 rounded-md mt-2">{content}</button>;
+}
+export default ButtonWhite;
