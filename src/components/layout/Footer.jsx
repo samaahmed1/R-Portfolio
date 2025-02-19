bccd70f8f19bd3e487d06249531ed75f5b704e99
samaahmed1 <sama34777@gmail.com>
2025-02-19 21:55:23 +0200

    Add: Footer Component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer () {
+  return (
+    <footer className="bg-[#212529]">
+      <div className="container px-5 mx-auto py-1">
+      <p className="text-center text-white text-lg">@RidhoSatriawan</p>
+      </div>
+    </footer>
+  )
+  }
+  export default Footer;
